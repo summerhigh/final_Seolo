977201cba96d73abd655e1ab314155db03b8d1eb
jinha Jang <jhgh0825@gmail.com>
2021-12-19 23:11:57 +0900

    추가항목에 대한 작업

--- FinalProject/src/com/seolo/check/IPlusDAO.java
+++ FinalProject/src/com/seolo/check/IPlusDAO.java
@@ -6,11 +6,17 @@ import java.util.ArrayList;
 public interface IPlusDAO
 {
 	//○ '추가항목'에 관한 작업(등록, 수정, 삭제...) 시 사용
-	public int add(PlusDTO dto) throws SQLException;
-	public int modify(PlusDTO dto) throws SQLException;
-	public int remove(String type, int plusNo) throws SQLException;
 	
-	public PlusDTO searchId(String type, int plusNo) throws SQLException;
-	public ArrayList<PlusDTO> plusList(int checkNo) throws SQLException;
+	public int addTime(PlusDTO dto) throws SQLException;
+	public int addScore(PlusDTO dto) throws SQLException;
+	public int modifyTime(PlusDTO dto) throws SQLException;
+	public int modifyScore(PlusDTO dto) throws SQLException;
+	public int removeTime(int plusNo) throws SQLException;
+	public int removeScore(int plusNo) throws SQLException;
+	
+	public PlusDTO searchTimeId(int plusNo) throws SQLException;
+	public PlusDTO searchScoreId(int plusNo) throws SQLException;
+	public ArrayList<PlusDTO> plusTimeList(int checkNo) throws SQLException;
+	public ArrayList<PlusDTO> plusScoreList(int checkNo) throws SQLException;
 	
 }
---
+++ FinalProject/src/com/seolo/check/StickerDTO.java
@@ -0,0 +1,25 @@
+package com.seolo.check;
+
+public class StickerDTO
+{
+	private int stickerNo;
+	private String content;
+
+	public int getStickerNo()
+	{
+		return stickerNo;
+	}
+	public void setStickerNo(int stickerNo)
+	{
+		this.stickerNo = stickerNo;
+	}
+	public String getContent()
+	{
+		return content;
+	}
+	public void setContent(String content)
+	{
+		this.content = content;
+	}
+
+}
